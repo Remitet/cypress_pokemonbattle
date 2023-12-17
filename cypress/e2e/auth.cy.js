8e2728d740792650fdacabe5640e6b2b10bd7ece
Тимур Калинин <t.kalinin@polymatica.com>
2023-12-17 11:08:18 +0300

    Изменение кавычек

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,56 +1,56 @@
-import { AuthPage } from "../../helpers/auth.page";
+import { AuthPage } from '../../helpers/auth.page';
 
-describe("Тесты авторизации", () => {
+describe('Тесты авторизации', () => {
   const pageAuth = new AuthPage();
 
   beforeEach(() => {
-    cy.intercept("POST", "trainers/auth").as("postAuth");
-    cy.visit("/");
+    cy.intercept('POST', 'trainers/auth').as('postAuth');
+    cy.visit('/');
   });
 
-  it("Авторизация с корректными логином/паролем", () => {
+  it('Авторизация с корректными логином/паролем', () => {
     pageAuth.typeInField(0, Cypress.env().login);
     pageAuth.typeInField(1, Cypress.env().password);
-    pageAuth.clickButton("buttonEnter");
-    cy.wait("@postAuth").its("response.statusCode").should("eq", 200);
+    pageAuth.clickButton('buttonEnter');
+    cy.wait('@postAuth').its('response.statusCode').should('eq', 200);
   });
 
   [
     [
-      "Авторизация с пустыми логином/паролем",
-      "{backspace}",
-      "{backspace}",
+      'Авторизация с пустыми логином/паролем',
+      '{backspace}',
+      '{backspace}',
       [
-        [0, "Введите почту"],
-        [1, "Введите пароль"],
+        [0, 'Введите почту'],
+        [1, 'Введите пароль'],
       ],
     ],
     [
-      "Авторизация с неккорректной почтой",
-      "Autotest",
+      'Авторизация с неккорректной почтой',
+      'Autotest',
       Cypress.env().password,
-      [[0, "Введите почту"]],
+      [[0, 'Введите почту']],
     ],
     [
-      "Авторизация с неккоректным паролем",
+      'Авторизация с неккоректным паролем',
       Cypress.env().login,
-      "Autotest",
-      [[2, "Неверные логин или пароль"]],
+      'Autotest',
+      [[2, 'Неверные логин или пароль']],
     ],
   ].forEach((type) => {
-    it(type[0], { tags: "@smoke" }, () => {
+    it(type[0], { tags: '@smoke' }, () => {
       pageAuth.typeInField(0, type[1]);
       pageAuth.typeInField(1, type[2]);
-      pageAuth.clickButton("buttonEnter");
+      pageAuth.clickButton('buttonEnter');
 
-      if (type[0].includes('Авторизация с неккоректным паролем"'))
-        cy.wait("@postAuth").its("response.statusCode").should("eq", 400);
+      if (type[0].includes('Авторизация с неккоректным паролем''))
+        cy.wait('@postAuth').its('response.statusCode').should('eq', 400);
 
       type[3].forEach((error) => {
-        pageAuth.checkElemText("errorText", error[1], error[0]);
+        pageAuth.checkElemText('errorText', error[1], error[0]);
         pageAuth.checkCssForValue(
-          "errorText",
-          "color",
+          'errorText',
+          'color',
           pageAuth.base.error_color,
           error[0]
         );
